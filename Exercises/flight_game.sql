fb3c7f6a6afce25e16dd15d7db9a9cdb28db0831
Aki78 <kmorooka78@gmail.com>
2022-08-25 23:47:04 +0300

    all is running (exercise 4_5)

--- Exercises/flight_game.sql
+++ Exercises/flight_game.sql
@@ -1,37 +1,41 @@
 drop database flight_game;
+
 create database flight_game;
 use flight_game;
 
+set foreign_key_checks=0;
+
 create table airport(
 id int,
-ident varchar(999),
-type varchar(999),
-name varchar(999),
+ident varchar(200),
+type varchar(200),
+name varchar(200),
 latitude_deg float(53),
 longitude_deg float(53),
 elevation_ft int,
-continent varchar(999),
-iso_country varchar(999),
-iso_region varchar(999),
-municipality varchar(999),
-scheduled_service varchar(999),
-gps_code varchar(999),
-iata_code varchar(999),
-local_code varchar(999),
-home_link varchar(999),
-wikipedia_link varchar(999),
-keywords varchar(999)
+continent varchar(200),
+iso_country varchar(200) not null,
+iso_region varchar(200),
+municipality varchar(200),
+scheduled_service varchar(200),
+gps_code varchar(200),
+iata_code varchar(200),
+local_code varchar(200),
+home_link varchar(200),
+wikipedia_link varchar(200),
+keywords varchar(200),
+primary key (ident)
 );
 
 create table goal(
 id int not null,
-name varchar(999),
-description varchar(999),
-icon varchar(999),
-target varchar(999),
+name varchar(200),
+description varchar(200),
+icon varchar(200),
+target varchar(200),
 target_minvalue float(53),
 target_maxvalue float(53),
-target_text varchar(999),
+target_text varchar(200),
 primary key (id)
 );
 
@@ -39,8 +43,8 @@ create table game(
 id int not null,
 co2_consumed float(53),
 co2_budget float(53),
-screen_name varchar(999),
-location varchar(999),
+screen_name varchar(200),
+location varchar(200),
 primary key (id)
 );
 
@@ -56,21 +60,21 @@ foreign key (game_id) references game(id)
 
 create table country(
 id int not null,
-code varchar(999),
-name varchar(999),
-continent varchar(999),
-wikipedia_link varchar(999),
-keywords varchar(999)
+code varchar(200),
+name varchar(200),
+continent varchar(200),
+wikipedia_link varchar(200),
+keywords varchar(200)
 );
 
-load data local infile '/home/aki/DatabaseCourse/Exercises/airports.csv'
+load data local infile './airports.csv'
 into table airport
 character set utf8
 fields terminated by ','
 lines terminated by '\n'
 ignore 1 lines;
 
-load data local infile '/home/aki/DatabaseCourse/Exercises/countries.csv'
+load data local infile './countries.csv'
 into table airport
 character set utf8
 fields terminated by ','
@@ -80,27 +84,24 @@ ignore 1 lines;
 alter table country
 drop column id;
 alter table country
-change code iso_country varchar(999);
+change code iso_country varchar(200);
 alter table country
 add primary key(iso_country);
 
 alter table airport
-add foreign key (iso_country)
-references country (iso_country);
+add foreign key (iso_country) references country (iso_country);
 
 alter table game
 add foreign key (location)
 references airport (ident);
 
-insert into game(id, co2_consumed, co2_budget, location)
-values
-(1, 2000, 10 000, ”Heini”, ”EFHK”),
-(2, 3000, 10 000, ”Vesa”, ”EGCC” ),
-(3, 8000, 10 000, ”Ilkka”, ”EGKK”);
+insert into game (id, co2_consumed, co2_budget, screen_name, location)
+values (1, 2000, 10000, 'Heini', 'EFHK'),
+(2, 3000, 10000, 'Vesa', 'EGCC' ),
+(3, 8000, 10000, 'Ilkka', 'EGKK');
 
-insert into game_reached(goal_id, game_id)
-values
-(1,4),
+insert into goal_reached(goal_id, game_id)
+values (1,4),
 (1,7),
 (2,4),
 (3,7);
