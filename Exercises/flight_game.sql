467ec3bbc9e378808c69b46c410689fe81c7872c
Aki78 <kmorooka78@gmail.com>
2022-08-25 18:11:59 +0300

    continue at home

--- Exercises/flight_game.sql
+++ Exercises/flight_game.sql
@@ -1,3 +1,4 @@
+drop database flight_game;
 create database flight_game;
 use flight_game;
 
@@ -52,6 +53,7 @@ foreign key (goal_id) references goal(id),
 foreign key (game_id) references game(id)
 
 );
+
 create table country(
 id int not null,
 code varchar(999),
@@ -59,4 +61,46 @@ name varchar(999),
 continent varchar(999),
 wikipedia_link varchar(999),
 keywords varchar(999)
-)
+);
+
+load data local infile '/home/aki/DatabaseCourse/Exercises/airports.csv'
+into table airport
+character set utf8
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile '/home/aki/DatabaseCourse/Exercises/countries.csv'
+into table airport
+character set utf8
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 lines;
+
+alter table country
+drop column id;
+alter table country
+change code iso_country varchar(999);
+alter table country
+add primary key(iso_country);
+
+alter table airport
+add foreign key (iso_country)
+references country (iso_country);
+
+alter table game
+add foreign key (location)
+references airport (ident);
+
+insert into game(id, co2_consumed, co2_budget, location)
+values
+(1, 2000, 10 000, ”Heini”, ”EFHK”),
+(2, 3000, 10 000, ”Vesa”, ”EGCC” ),
+(3, 8000, 10 000, ”Ilkka”, ”EGKK”);
+
+insert into game_reached(goal_id, game_id)
+values
+(1,4),
+(1,7),
+(2,4),
+(3,7);
