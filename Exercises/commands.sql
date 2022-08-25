61a6d91fec208a70cd73ce6918fd53911990b4b0
Aki78 <kmorooka78@gmail.com>
2022-08-25 18:05:36 +0300

    done exercise 4_5, next: final check

--- Exercises/commands.sql
+++ Exercises/commands.sql
@@ -1,4 +1,44 @@
-use flight_game
-show tables
+use flight_game;
+show tables;
 
-LOAD DATA INFILE 'airports.csv'
+load data local infile '/home/aki/DatabaseCourse/Exercises/airports.csv'
+into table airport
+character set utf8
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile '/home/aki/DatabaseCourse/Exercises/countries.csv'
+into table airport
+character set utf8
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 lines;
+
+alter table countries
+drop column id;
+alter table country
+change code iso_country varchar(999);
+alter table country
+add primary key(iso_country);
+
+alter table airport
+add foreign key (iso_country)
+references countries (iso_country);
+
+alter table game
+add foreign key (location)
+references airport (ident);
+
+insert into game(id, co2_consumed, co2_budget, location)
+values
+(1, 2000, 10 000, ”Heini”, ”EFHK”),
+(2, 3000, 10 000, ”Vesa”, ”EGCC” ),
+(3, 8000, 10 000, ”Ilkka”, ”EGKK”);
+
+insert into game_reached(goal_id, game_id)
+values
+(1,4),
+(1,7),
+(2,4),
+(3,7);
--- Exercises/flight_game.sql
+++ Exercises/flight_game.sql
@@ -38,11 +38,10 @@ create table game(
 id int not null,
 co2_consumed float(53),
 co2_budget float(53),
+screen_name varchar(999),
 location varchar(999),
 primary key (id)
 );
-
--- foreign key (location)
 
 create table goal_reached(
 goal_id int not null,
@@ -61,35 +60,3 @@ continent varchar(999),
 wikipedia_link varchar(999),
 keywords varchar(999)
 )
-
--- ========================================
--- create table duckburger(
--- ID int not null auto_increment,
--- first_name varchar(40),
--- last_name varchar(40),
--- primary key (id)
--- );
-
--- create table pet(
--- ID int not null auto_increment,
--- name varchar(40),
--- primary key(id)
--- );
-
--- create table owns(
--- pet_ID int,
--- duckburger_ID int,
--- primary key (pet_ID, duckburger_ID),
--- foreign key (pet_ID) references pet(ID),
--- foreign key (duckburger_ID) references duckburger(ID)
--- );
-
--- insert into duckburger(first_name, last_name)
--- values("Donald", "Duck"),("Scrooge", "McDuck"),
--- ("Huey", "Duck"),("Magica", "De Spell"), ("Mickey", "Mouse");
-
--- insert into pet(name)
--- values("Bolivar"), ("Pluto"), ("Ratface");
-
--- insert into owns(pet_ID, duckburger_ID)
--- values(1,1),(1,3),(2,5),(3,4);
